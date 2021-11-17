826dd75ef478ae8df079cd449090f62dc1906e03
brucebe <bebert2git config --global user.email bebert@udesa.edu.argit config --global user.email bebert2>
2021-11-17 15:29:42 -0300

    js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,21 @@
-// Home
+// Home // 
+
+let url = ("https://api.themoviedb.org/3/movie/76341?api_key=a14cb1fc6fe760d5cbd8a057690f449d")
+
+fetch (url)
+.then(function(response){
+    return response.jason();
+})
+.then(function(data){
+    console.log(data);
+})
+.catch(function(error){
+    console.log('El error fue: ' + error)
+})
+
+
+
+let url2 = ("") 
 
 fetch ()
 .then(function(response){
@@ -23,14 +40,3 @@ fetch ()
     console.log('El error fue: ' + error)
 })
 
-
-fetch ()
-.then(function(response){
-    return response.jason();
-})
-.then(function(data){
-    console.log(data);
-})
-.catch(function(error){
-    console.log('El error fue: ' + error)
-})
