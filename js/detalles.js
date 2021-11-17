9ebfe4f466c8e6dbace4e43af9a28f2e368bdde6
brucebe <bebert2git config --global user.email bebert@udesa.edu.argit config --global user.email bebert2>
2021-11-17 09:07:21 -0300

    js

---
+++ js/detalles.js
@@ -0,0 +1,36 @@
+// Home
+
+fetch ()
+.then(function(response){
+    return response.jason();
+})
+.then(function(data){
+    console.log(data);
+})
+.catch(function(error){
+    console.log('El error fue: ' + error)
+})
+
+
+fetch ()
+.then(function(response){
+    return response.jason();
+})
+.then(function(data){
+    console.log(data);
+})
+.catch(function(error){
+    console.log('El error fue: ' + error)
+})
+
+
+fetch ()
+.then(function(response){
+    return response.jason();
+})
+.then(function(data){
+    console.log(data);
+})
+.catch(function(error){
+    console.log('El error fue: ' + error)
+})
