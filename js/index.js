3240ada18f4cf622c28749671c2291bae949e175
brucebe <bebert2git config --global user.email bebert@udesa.edu.argit config --global user.email bebert2>
2021-11-17 16:28:13 -0300

    js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 // Home // 
+// Peliculas populares //
 
 let url = ("https://api.themoviedb.org/3/movie/76341?api_key=a14cb1fc6fe760d5cbd8a057690f449d")
 
@@ -6,18 +7,22 @@ fetch (url)
 .then(function(response){
     return response.jason();
 })
-.then(function(data){
+.then(function(data) {
+    for (let i= 0; i<4; i++){
+        console.log('' + i)
+
+    }
     console.log(data);
 })
 .catch(function(error){
     console.log('El error fue: ' + error)
 })
 
-
+// Series populares //
 
 let url2 = ("") 
 
-fetch ()
+fetch (url2)
 .then(function(response){
     return response.jason();
 })
@@ -28,8 +33,11 @@ fetch ()
     console.log('El error fue: ' + error)
 })
 
+// Peliculas mas valoradas //
 
-fetch ()
+let url3 = ("")
+
+fetch (url3)
 .then(function(response){
     return response.jason();
 })
