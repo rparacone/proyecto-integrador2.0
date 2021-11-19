a78b496f9042d3b135e2c55192e1f227fa8e0e57
brucebe <bebert2git config --global user.email bebert@udesa.edu.argit config --global user.email bebert2>
2021-11-19 19:19:54 -0300

    js hipervinculos y mas correcciones

--- js/index.js
+++ js/index.js
@@ -11,12 +11,29 @@ fetch ( url + '/movie/popular?api_key='+ api_key + language)
     return response.json();
 })
 .then(function(data){
-    //Trae un elemento de HTML del documento al que pertenece el archivo segun su id
-    console.log(data);
+    //Capturo el Dom
+    console.log(data.results);
+    let info = data.results;
+
+    let lista = document.querySelector('lista')
+    let elementolista = '';
+    
     for(i = 0; i < 5; i++){
-       
-        document.getElementById("peliculas").innerHTML +='<div class = "flexHijo" ><a href=""> <h3>'+ data.results[i].title + ' </h3> <img src="https://image.tmdb.org/t/p/original/' + data.results[i].poster_path +'" alt="Accion"/></a></div>';
-        
+        elementolista += `<article>
+                                img scr=${info[i].image}>
+                                <p> Nombre:`
+    
+
+
+        document.getElementById("peliculas").innerHTML +=
+        `
+        <div class = "flexHijo" ><a href="detallepeliculas"><h3>${data.results[i].title}
+        </h3><img src="https://image.tmdb.org/t/p/original/${data.results[i].poster_path}" 
+        alt="Accion"/>
+        </a>
+        </div>
+        `
+        ;
     }
 })
 .catch(function(error){
@@ -32,8 +49,13 @@ fetch ( url + "/tv/popular?api_key=" + api_key + language)
     //Trae un elemento de HTML del documento al que pertenece el archivo segun su id
     console.log(data);
     for(i = 0; i < 5; i++){
-
-        document.getElementById("series").innerHTML +='<div class = "flexHijo" ><a href=""> <h3>'+ data.results[i].name + ' </h3> <img src="https://image.tmdb.org/t/p/original/' + data.results[i].poster_path +'" alt="Accion"/></a></div>';
+        document.getElementById("series").innerHTML +=
+        `
+        <div class = "flexHijo" ><a href="detallegeneros"><h3>${data.results[i].name}
+        </h3> <img src="https://image.tmdb.org/t/p/original/${data.results[i].poster_path}"
+        alt="Accion"/></a></div>
+        `
+        ;
     }
 })
 .catch(function(error){
@@ -49,58 +71,32 @@ fetch ( url + "/genre/movie/list?api_key=" + api_key + language)
 .then(function(data){
     //Trae un elemento de HTML del documento al que pertenece el archivo segun su id
     console.log(data);
-    for(i = 0; i < 5; i++){
-
-        document.getElementById("generos").innerHTML +='<div class = "flexHijo" ><a href=""> <h3>'+ data.results[i].name + ' </h3> <img src="https://image.tmdb.org/t/p/original/' + data.results[i].poster_path +'" alt="Accion"/></a></div>';
+    for(i = 0; i < 5; i++)
+    // Capturar elementos del DOM
+    lista = document.querySelector('lista')
+    let elementolista = '';
+    
+    for(i=0; i<5; i++){
+        elementolista += `<article>
+                                <img src=${info[i].image}>
+                                <p> nombre: ${info[i].name}
+                                `
+                                 
     }
+    {
+
+        document.getElementById("generos").innerHTML +=
+        `
+        <div class ="flexHijo">
+        <a href="">
+        <h3>${data.results[i].name}</h3>
+        <img src="https://image.tmdb.org/t/p/original/${data.results[i].poster_path}"
+        alt="Accion"/></a></div>
+        `
+        ;
+
+    }  
 })
 .catch(function(error){
     console.log('El error fue: ' + error)
 })
-
-/*
-fetch (url)
-.then(function(response){
-    return response.jason();
-})
-.then(function(data) {
-    for (let i= 0; i<4; i++){
-        console.log('' + i)
-
-    }
-    console.log(data);
-})
-.catch(function(error){
-    console.log('El error fue: ' + error)
-})
-
-// Series populares
-
-let url2 = ("") 
-
-fetch (url2)
-.then(function(response){
-    return response.jason();
-})
-.then(function(data){
-    console.log(data);
-})
-.catch(function(error){
-    console.log('El error fue: ' + error)
-})
-
-// Peliculas mas valoradas
-
-let url3 = ("")
-
-fetch (url3)
-.then(function(response){
-    return response.jason();
-})
-.then(function(data){
-    console.log(data);
-})
-.catch(function(error){
-    console.log('El error fue: ' + error)
-})
-*/
--- js/peliculas.js
+++ js/peliculas.js
@@ -1,14 +1,32 @@
+// Home
+const url = 'https://api.themoviedb.org/3' // la direccion de la api. despues agregamos los recursos de cada api. 
+const api_key = "05c046159c1ed93371b2e717b946f722"
+const language = "&language=en-US"
+
+// Peliculas populares
+
+// BUSQUEDA de peliculas -- Asignando el valor
 fetch ( url + '/movie/popular?api_key='+ api_key + language)
 .then(function(response){
     return response.json();
 })
 .then(function(data){
-    //Trae un elemento de HTML del documento al que pertenece el archivo segun su id
+    //Capturo el Dom
     console.log(data);
     for(i = 0; i < 5; i++){
-       
-        document.getElementById("peliculas").innerHTML +='<div class = "flexHijo" ><a href=""> <h3>'+ data.results[i].title + ' </h3> <img src="https://image.tmdb.org/t/p/original/' + data.results[i].poster_path +'" alt="Accion"/></a></div>';
-        
+    let lista = document.querySelector('lista')
+    let elementolista = '';
+
+
+        document.getElementById("peliculas").innerHTML +=
+        `
+        <div class = "flexHijo" ><a href="detallepeliculas1.html"><h3>${data.results[i].title}
+        </h3><img src="https://image.tmdb.org/t/p/original/${data.results[i].poster_path}" 
+        alt="Accion"/>
+        </a>
+        </div>
+        `
+        ;
     }
 })
 .catch(function(error){
